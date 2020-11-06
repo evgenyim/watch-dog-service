06d4b1eb9961a5491c5a0f6ca4e3b4c55a981a27
Evgeny Im <evgenyim6456@gmail.com>
2020-11-06 14:03:04 +0300

    add test for TrackingService

---
+++ src/Tests/TrackingserviceTests/TestService.cs
@@ -0,0 +1,24 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrackingserviceTests
+{
+    class TestServiceGood : Service
+    {
+        public override bool IsAlive()
+        {
+            return true;
+        }
+    }
+
+    class TestServiceBad : Service
+    {
+        public override bool IsAlive()
+        {
+            return false;
+        }
+    }
+
+}
